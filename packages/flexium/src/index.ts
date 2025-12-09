852a0136b67877be278b87f8252ffb811db43f90
Wick-Lim <wicklim90@gmail.com>
2025-12-09 22:16:03 +0900

    release: v0.8.15

--- packages/flexium/src/index.ts
+++ packages/flexium/src/index.ts
@@ -22,7 +22,7 @@
  * ```
  */
 
-export const VERSION = '0.8.14'
+export const VERSION = '0.8.15'
 
 // Intentionally empty - use explicit submodule imports
 // flexium/core, flexium/dom, flexium/canvas, flexium/primitives, flexium/router
