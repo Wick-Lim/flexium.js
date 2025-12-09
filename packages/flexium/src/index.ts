67e0e3a9396212b093825a2a717546bc3163f759
dsauto-junhyuk <jun-hyuk02@dsauto.kr>
2025-12-09 09:08:17 +0900

    release: v0.8.0

--- packages/flexium/src/index.ts
+++ packages/flexium/src/index.ts
@@ -22,7 +22,7 @@
  * ```
  */
 
-export const VERSION = '0.7.16'
+export const VERSION = '0.8.0'
 
 // Intentionally empty - use explicit submodule imports
 // flexium/core, flexium/dom, flexium/canvas, flexium/primitives, flexium/router
