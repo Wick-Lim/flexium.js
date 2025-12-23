5e4e20fbe4e3137e9d5af9a249da418d8d9e6fa2
Wick-Lim <wicklim90@gmail.com>
2025-12-23 22:39:39 +0900

    release: v0.16.1

--- packages/flexium-canvas/src/index.ts
+++ packages/flexium-canvas/src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.16.0'
+export const VERSION = '0.16.1'
 
 // Use subpaths for actual functionality:
 // - flexium-canvas/dom: Canvas, DrawRect, DrawCircle, DrawArc, DrawLine, DrawPath, DrawText
